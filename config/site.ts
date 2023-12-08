b44a81c18f5752fa26f0270832067070df35349f
Rick Blalock <rblalock@gmail.com>
2023-12-08 13:14:52 -0500

    Hello world

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "1982 vs. the future",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Beat the spy from the future.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/rblalock",
+    github: "https://github.com/rblalock"
   },
 }
